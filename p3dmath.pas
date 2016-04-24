3c7d33ba0c9170c0e02d827897c7f0cff982a19c
soerensen3 <johannes.soerensen@gmail.com>
2016-04-24 16:36:43 +0200

    cleanup and new quaternion functions

--- p3dmath.pas
+++ p3dmath.pas
@@ -40,76 +40,71 @@ type
 
   { TVec2 }
   {$DEFINE INTERFACE}
+
   {$DEFINE TVECTOR:= TVec2}
   {$DEFINE NUMFIELD:=2}
   {$INCLUDE p3dmath_vector.inc}
 
   { TVec3 }
-  {$DEFINE INTERFACE}
   {$DEFINE TVECTOR:= TVec3}
   {$DEFINE NUMFIELD:=3}
   {$INCLUDE p3dmath_vector.inc}
 
-  { TVec3 }
-  {$DEFINE INTERFACE}
+  { TVec4 }
   {$DEFINE TVECTOR:= TVec4}
   {$DEFINE NUMFIELD:=4}
   {$INCLUDE p3dmath_vector.inc}
 
   { TMat2 }
-  {$DEFINE INTERFACE}
   {$DEFINE TMATRIX:= TMat2}
   {$DEFINE TVECTOR:= TVec2}
   {$DEFINE NUMFIELD:=2}
   {$INCLUDE p3dmath_matrix.inc}
 
   { TMat3 }
-  {$DEFINE INTERFACE}
   {$DEFINE TMATRIX:= TMat3}
   {$DEFINE TVECTOR:= TVec3}
   {$DEFINE NUMFIELD:=3}
   {$INCLUDE p3dmath_matrix.inc}
 
   { TMat4 }
-  {$DEFINE INTERFACE}
   {$DEFINE TMATRIX:= TMat4}
   {$DEFINE TVECTOR:= TVec4}
   {$DEFINE NUMFIELD:=4}
   {$INCLUDE p3dmath_matrix.inc}
 
-  //CONSTRUCTORS
-  {$INCLUDE p3dmath_vector_constructors.inc}
-  {$INCLUDE p3dmath_matrix_constructors.inc}
-
-  //HELPER FUNCTIONS
-  { TVec2 }
-  {$DEFINE TVECTOR:= TVec2}
-  {$DEFINE NUMFIELD:=2}
-  {$DEFINE HELPERFUNCT}
-  {$INCLUDE p3dmath_vector.inc}
-
-  { TVec3 }
-  {$DEFINE TVECTOR:= TVec3}
-  {$DEFINE NUMFIELD:=3}
-  {$DEFINE HELPERFUNCT}
-  {$INCLUDE p3dmath_vector.inc}
-
-  { TVec4 }
-  {$DEFINE TVECTOR:= TVec4}
-  {$DEFINE NUMFIELD:=4}
-  {$DEFINE HELPERFUNCT}
-  {$INCLUDE p3dmath_vector.inc}
-
   { TQuaternion }
-  {$DEFINE INTERFACE}
   {$DEFINE TVECTOR:= TVec4}
   {$INCLUDE p3dmath_quaternion.inc}
 
   {$INCLUDE p3dmath_consts.inc}
 
-  {Type Arrays}
-  {$DEFINE INTERFACE}
+  //CONSTRUCTORS
+  {$INCLUDE p3dmath_vector_constructors.inc}
+  {$INCLUDE p3dmath_matrix_constructors.inc}
 
+  { TVec2 }
+  {$DEFINE TVECTOR:= TVec2}
+  {$DEFINE NUMFIELD:=2}
+  //HELPER FUNCTIONS AND OPERATORS
+  {$INCLUDE p3dmath_vector_functions.inc}
+  {$INCLUDE p3dmath_vector_op.inc}
+
+  { TVec3 }
+  {$DEFINE TVECTOR:= TVec3}
+  {$DEFINE NUMFIELD:=3}
+  //HELPER FUNCTIONS AND OPERATORS
+  {$INCLUDE p3dmath_vector_functions.inc}
+  {$INCLUDE p3dmath_vector_op.inc}
+
+  { TVec4 }
+  {$DEFINE TVECTOR:= TVec4}
+  {$DEFINE NUMFIELD:=4}
+  //HELPER FUNCTIONS AND OPERATORS
+  {$INCLUDE p3dmath_vector_functions.inc}
+  {$INCLUDE p3dmath_vector_op.inc}
+
+  {Type Arrays}
   {$INCLUDE p3dmath_arrays_decl.inc}
 
   {$UNDEF INTERFACE}
@@ -118,7 +113,6 @@ implementation
 
 {$DEFINE IMPLEMENTATION}
 {$INCLUDE p3dmath_vector_constructors.inc}
-{$DEFINE IMPLEMENTATION}
 {$INCLUDE p3dmath_matrix_constructors.inc}
 
 { TVec2 }
